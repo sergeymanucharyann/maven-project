0ac73fe5ec152d1a2c0bb242c928ccb63cb26c25
Sergey <sergeymanucharyan5@gmail.com>
2023-03-18 04:50:32 -0800

    Maven Project

---
+++ src/main/java/com/bdg/Main.java
@@ -0,0 +1,7 @@
+package com.bdg;
+import org.example.ArrayHomework;
+public class Main {
+    public static void main(String[] args) {
+        ArrayHomework arrayHomework = new ArrayHomework();
+    }
+}
