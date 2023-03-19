b1aa57f34d0105f2833b989c949111c1d9a19bc6
Sergey <sergeymanucharyan5@gmail.com>
2023-03-19 04:06:24 -0800

    Add Object of 'MethodsHomework' class

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -1,7 +1,10 @@
 package com.bdg;
 import org.example.ArrayHomework;
+import org.example.MethodsHomework;
+
 public class Main {
     public static void main(String[] args) {
+        MethodsHomework methodsHomework = new MethodsHomework();
         ArrayHomework arrayHomework = new ArrayHomework();
     }
 }
